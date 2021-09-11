1ccc163b276d175a6ea1de7ad3e591f28d16899e
ryanpenber <rainpenber@gmail.com>
2021-09-12 05:14:33 +0800

    change

--- README.md
+++ README.md
@@ -1,88 +1,60 @@
-![sazs34’s github stats](https://github-readme-stats.vercel.app/api?username=sazs34&show_icons=true&theme=merko)
+# 特别声明:
 
-# MyActions
-自己用来签到的东东,不支持售后
+-   本仓库发布的 MyActions 项目中涉及的任何解锁和解密分析脚本，仅用于测试和学习研究，禁止用于商业用途，不能保证其合法性，准确性，完整性和有效性，请根据情况自行判断.
 
-更新时间:2020-8-31 18:35:00
+-   本项目内所有资源文件，禁止任何公众号、自媒体进行任何形式的转载、发布。
 
-##
-目前已支持[@NobyDa](https://github.com/NobyDa) 以及[@lxk0301](https://github.com/lxk0301) 中京东签到的内容,优点是支持无限数量的京东cookie
+-   sazs34 对任何脚本问题概不负责，包括但不限于由任何脚本错误导致的任何损失或损害.
+
+-   间接使用脚本的任何用户，包括但不限于建立 VPS 或在某些行为违反国家/地区法律或相关法规的情况下进行传播, sazs34 对于由此引起的任何隐私泄漏或其他后果概不负责.
+
+-   请勿将 MyActions 项目的任何内容用于商业或非法目的，否则后果自负.
+
+-   如果任何单位或个人认为该项目的脚本可能涉嫌侵犯其权利，则应及时通知并提供身份证明，所有权证明，我们将在收到认证文件后删除相关脚本.
+
+-   任何以任何方式查看此项目的人或直接或间接使用该 MyActions 项目的任何脚本的使用者都应仔细阅读此声明。sazs34 保留随时更改或补充此免责声明的权利。一旦使用并复制了任何相关脚本或 MyActions 项目的规则，则视为您已接受此免责声明.
+
+    **您必须在下载后的 24 小时内从计算机或手机中完全删除以上内容.** </br>
+
+    > **_您使用或者复制了本仓库且本人制作的任何脚本，则视为`已接受`此声明，请仔细阅读_**
+
+
+# 说明:
+
+鉴于 lxk0301 大佬的分支因为 fork 过多用于执行 actions 导致被删，为了防范于未然
+
+> 麻烦各位**不要 fork**`不要fork` _不要 fork_ 不要 fork [不要 fork](backup/reposync.md) ,通过下面的方法重新创建分支，同步代码
+
+[此方式亲测可行，请放心食用，点击查看如何使用 reposync 的方式进行同步代码](backup/reposync.md)
+
+[此方式亲测可行，请放心食用，点击查看如何使用 reposync 的方式进行同步代码](backup/reposync.md)
+
+[此方式亲测可行，请放心食用，点击查看如何使用 reposync 的方式进行同步代码](backup/reposync.md)
+
 
 ## 使用教程
 
-1. 直接fork走
+1. [按照这个教程进行 reposync](backup/reposync.md)
 2. 再在`Settings`-`Secrets`里面添加`JD_COOKIE`
-3. 多条cookie用`&`隔开，支持无数条cookie
+3. 多条 cookie 用`&`隔开，支持无数条 cookie
+4. 前三步之后，点击一下右上角的 star（fork 左边那个），让 workflow 运行一次。
 
-上面三步搞定后就不用管了
+上面四步搞定后就不用管了。
 
-刚fork完可能在Actions中看不到对应的workflow
+更多Secrets配置[点击查看](backup/secrets.md)
 
-目前已配置好自动执行时间，到了指定时间会执行，并且看到workflow
 
-### Secrets全集合
+> 具体如何取 cookie 如何配置,可参考 [lxk0301 的获取京东Cookies教程](https://github.com/lxk0301/jd_scripts/blob/master/githubAction.md#%E4%BA%AC%E4%B8%9Ccookie)
 
-#### `JD_COOKIE`
 
-> 【必须】京东Cookie，必须有这个，否则全部不执行
+## 鸣谢
 
-多个账号间用&隔开，支持无数个账号签到
+[@NobyDa](https://github.com/NobyDa) - 京东每日签到
 
-#### `PUSH_KEY` 
+[@lxk0301](https://github.com/lxk0301) - 京东系列其他签到
 
-> 【可选】[server酱的微信通知](http://sc.ftqq.com/3.version)服务
+[@yangtingxiao](https://github.com/yangtingxiao) - 京东排行榜，天天提额
 
-用于推送Cookie失效通知，同时用于推送京东农场兑换礼物通知等
+[@Zero-S1](https://github.com/Zero-S1/xmly_speed) - 喜马拉雅极速版签到
 
-#### `BARK_PUSH`
-
-> 【可选】BARK这个手机APP的推送 https://t.me/jdfruit/80
-
-在settings->secrets->new secret里面Name填写BARK_PUSH，Value填写app提供的token
-
-(注：此token是https://api.day.app/后面的内容)
-
-#### `FruitShareCodes` 
-
-> 【可选】京东农场分享码
-
-```javascript
-// 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间按Cookie隔开方法,即用&符号隔开,下面给一个示例
-// 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
-0a74407df5df4fa99672a037eec61f7e@dbb21614667246fabcfd9685b6f448f3@6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6&6fbd26cc27ac44d6a7fed34092453f77@61ff5c624949454aa88561f2cd721bf6
-```
-
-#### `JDMarketCoinToBeans`
-
-> 【可选】京小超蓝币换京东个数,请填入纯数字,并且在0~20之间
-
-如果值超出范围会直接使用0,不用担心脚本无法正常执行
-
-#### `JDJoyFeedCount`
-
-> 【可选】宠汪汪喂食数量，请填写[10,20,40,80]其中任意一个
-
-如果值超出范围会直接使用10,不用担心脚本无法正常执行
-
-### Cookie获取和配置
-
-> 具体如何取cookie如何配置,可参考 https://github.com/lxk0301/scripts/issues/8#issuecomment-675837338
-
-```
-
-针对京东cookie我们只需要
-pt_key=****;
-和
-pt_pin=***;
-的部分
-
-我有两个京东账号,则我JD_COOKIE里面要填写的内容为
-pt_key=****;pt_pin=***;&pt_key=****;pt_pin=***;
-```
-
-### fork后如何同步代码
-
-参考
-
-http://www.ibloger.net/article/3361.html
 
--- smartReplace.js
+++ smartReplace.js
@@ -1,67 +1,113 @@
-const download = require("download");
-async function replaceWithSecrets(content, Secrets) {
-    if (!Secrets || !Secrets) return content;
-    const replacements = [];
-    //此处为字符串,说明是传入指定cookie信息了,仅替换cookie即可,其它的不需要替换
-    if (typeof Secrets == "string") {
-        if (content.indexOf("require('./jdCookie.js')") > 0) {
-            replacements.push({ key: "require('./jdCookie.js')", value: `{CookieJD:'${Secrets}'}` });
-        }
-        if (content.indexOf("京东多合一签到") > 0 && content.indexOf("@NobyDa") > 0) {
-            replacements.push({ key: /var Key = ''/, value: `var Key = '${Secrets}'` });
-        }
-        return batchReplace(content, replacements);
-    }
-    if (Secrets.JD_COOKIE && content.indexOf("require('./jdCookie.js')") > 0) {
-        replacements.push({ key: "require('./jdCookie.js')", value: JSON.stringify(Secrets.JD_COOKIE.split("&")) });
-    }
-    if (!Secrets.PUSH_KEY && !Secrets.BARK_PUSH) {
-        if (content.indexOf("require('./sendNotify')") > 0) {
-            replacements.push({ key: "require('./sendNotify')", value: "" });
-        }
-    } else {
-        await download_notify();
-    }
-    if (content.indexOf("jdFruitShareCodes") > 0) {
-        await download_jdFruit();
-    }
-    if (Secrets.MarketCoinToBeanCount && !isNaN(Secrets.MarketCoinToBeanCount)) {
-        let coinToBeanCount = parseInt(Secrets.MarketCoinToBeanCount);
-        if (coinToBeanCount >= 0 && coinToBeanCount <= 20 && content.indexOf("$.getdata('coinToBeans')") > 0) {
-            console.log("蓝币兑换京豆操作已注入");
-            replacements.push({ key: "$.getdata('coinToBeans')", value: coinToBeanCount });
-        }
-    }
-    if (Secrets.JoyFeedCount && !isNaN(Secrets.JoyFeedCount)) {
-        let feedCount = parseInt(Secrets.JoyFeedCount);
-        if ([10, 20, 40, 80].indexOf(feedCount) >= 0 && content.indexOf("$.getdata('joyFeedCount')") > 0) {
-            console.log("宠汪汪喂食操作已注入");
-            replacements.push({ key: "$.getdata('joyFeedCount')", value: feedCount });
-        }
-    }
-    return batchReplace(content, replacements);
+const axios = require("axios");
+const fs = require("fs");
+const replacements = [];
+var remoteContent;
+async function init(content) {
+    remoteContent = content;
+    await inject();
+    return batchReplace(remoteContent);
 }
-function batchReplace(content, replacements) {
+//#region 注入代码
+async function inject() {
+    await inject_jd();
+}
+
+async function inject_jd() {
+    if (!process.env.JD_COOKIE) return;
+    if (remoteContent.indexOf("function requireConfig()") >= 0 && remoteContent.indexOf("jd_bean_sign.js") >= 0) {
+        replacements.push({
+            key: "resultPath = err ? '/tmp/result.txt' : resultPath;",
+            value: `resultPath = err ? './tmp/result.txt' : resultPath;`,
+        });
+        replacements.push({
+            key: "JD_DailyBonusPath = err ? '/tmp/JD_DailyBonus.js' : JD_DailyBonusPath;",
+            value: `JD_DailyBonusPath = err ? './tmp/JD_DailyBonus.js' : JD_DailyBonusPath;`,
+        });
+        replacements.push({
+            key: "outPutUrl = err ? '/tmp/' : outPutUrl;",
+            value: `outPutUrl = err ? './tmp/' : outPutUrl;`,
+        });
+    }
+    await downloader_jd();
+    await downloader_notify();
+}
+
+function batchReplace() {
     for (var i = 0; i < replacements.length; i++) {
-        content = content.replace(replacements[i].key, replacements[i].value);
+        remoteContent = remoteContent.replace(replacements[i].key, replacements[i].value);
     }
-    return content;
+    // console.log(remoteContent);
+    return remoteContent;
+}
+//#endregion
+
+//#region 文件下载
+
+async function downloader_jd() {
+    if (/require\(['"`]{1}.\/jdCookie.js['"`]{1}\)/.test(remoteContent))
+        await download("https://github.com/lxk0301/jd_scripts/raw/master/jdCookie.js", "./jdCookie.js", "京东Cookies");
+    if (remoteContent.indexOf("jdFruitShareCodes") > 0) {
+        await download(
+            "https://github.com/lxk0301/jd_scripts/raw/master/jdFruitShareCodes.js",
+            "./jdFruitShareCodes.js",
+            "东东农场互助码"
+        );
+    }
+    if (remoteContent.indexOf("jdPetShareCodes") > 0) {
+        await download(
+            "https://github.com/lxk0301/jd_scripts/raw/master/jdPetShareCodes.js",
+            "./jdPetShareCodes.js",
+            "京东萌宠"
+        );
+    }
+    if (remoteContent.indexOf("jdPlantBeanShareCodes") > 0) {
+        await download(
+            "https://github.com/lxk0301/jd_scripts/raw/master/jdPlantBeanShareCodes.js",
+            "./jdPlantBeanShareCodes.js",
+            "种豆得豆互助码"
+        );
+    }
+    if (remoteContent.indexOf("jdSuperMarketShareCodes") > 0)
+        await download(
+            "https://github.com/lxk0301/jd_scripts/raw/master/jdSuperMarketShareCodes.js",
+            "./jdSuperMarketShareCodes.js",
+            "京小超互助码"
+        );
+    if (remoteContent.indexOf("jdFactoryShareCodes") > 0) {
+        await download(
+            "https://github.com/lxk0301/jd_scripts/raw/master/jdFactoryShareCodes.js",
+            "./jdFactoryShareCodes.js",
+            "东东工厂互助码"
+        );
+    }
+    if (remoteContent.indexOf("jdDreamFactoryShareCodes") > 0) {
+        await download(
+            "https://github.com/lxk0301/jd_scripts/raw/master/jdDreamFactoryShareCodes.js",
+            "./jdDreamFactoryShareCodes.js",
+            "京喜工厂互助码"
+        );
+    }
+    if (remoteContent.indexOf("jdJxStoryShareCodes") > 0) {
+        await download(
+            "https://github.com/lxk0301/jd_scripts/raw/master/jdJxStoryShareCodes.js",
+            "./jdJxStoryShareCodes.js",
+            "京喜故事互助码"
+        );
+    }
 }
 
-async function download_notify(){
-        await download("https://github.com/lxk0301/scripts/raw/master/sendNotify.js", "./", {
-            filename: "sendNotify.js",
-        });
-        console.log("下载通知代码完毕");
+async function downloader_notify() {
+    await download("https://github.com/lxk0301/jd_scripts/raw/master/sendNotify.js", "./sendNotify.js", "统一通知");
 }
 
-async function download_jdFruit(content) {
-    await download("https://github.com/lxk0301/scripts/raw/master/jdFruitShareCodes.js", "./", {
-            filename: "jdFruitShareCodes.js",
-        });
-        console.log("下载农场分享码代码完毕");
+async function download(url, path, target) {
+    let response = await axios.get(url);
+    let fcontent = response.data;
+    await fs.writeFileSync(path, fcontent, "utf8");
+    console.log(`下载${target}完毕`);
 }
+//#endregion
 
 module.exports = {
-    replaceWithSecrets,
+    inject: init,
 };
